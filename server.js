b7affd5ed1418d98369d17f0a9e3d8dad11c4beb
Debora Galeli <deboragaleli@gmail.com>
2018-10-11 22:25:43 -0400

    fix

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+
+const { buildSchema } = require('graphql');
+// const Routes = require('./routes/routes');
+
+const app = express();
+const PORT = 3000;
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+app.use('/graphql', graphqlHTTP({
+  graphiql: true,
+  schema
+}));
+
+//const token = 'YOUR TOKEN HERE';
+//const client = createClient(token);
+
+app.get('/', (request, response) => {
+  return response.json({
+    msg: 'Welcome to Classic Pairings'
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running at PORT ${PORT}');
+});
