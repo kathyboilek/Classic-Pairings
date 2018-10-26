0a2f92c3a450338786dd27c9fdeeb4c74c300623
Kathleen Boilek <kaboilek@gmail.com>
2018-10-26 14:26:51 -0400

    commented out all references referring to express

--- routes/api/saved.js
+++ routes/api/saved.js
@@ -1,4 +1,4 @@
-const router = require("express").Router();
+// const router = require("express").Router();
 const pairingController = require("../../controllers/pairingController");
 
 router.route("/")
