b47f1dc882abce13692c8f3a804ca2e9309816ef
Debora Galeli <deboragaleli@gmail.com>
2018-10-26 12:16:05 -0400

    fix auth0 logic

---
+++ client/src/pages/ResultsPage/results.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './results.css';
+import Nav from '../../components/NavBar';
+import Footer from '../../components/Footer';
+
+class Results extends Component {
+  render() {
+    return (
+      <div className="Results">
+        <Nav />
+        
+          <div className="storage">
+              <div className="capsule">
+                <div className="hero-banner"></div> 
+                <div className="results-cards"></div>  
+              </div> 
+          </div>
+
+        <Footer />  
+      </div>
+    );
+  }
+}
+
+export default Results;
